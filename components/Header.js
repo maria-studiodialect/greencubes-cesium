baec3b11a236e8bfb0a8c60505f1c8575d3f73c8
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2023-05-22 08:51:54 +0100

    widgets

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <>
         <div className="fixed top-0 left-0 z-10 bg-black/60 text-white flex justify-between items-center w-full py-5 px-10 font-extralight backdrop-blur">
-            <div className="relative"><Image alt='logo' src='/img/R-Evolution Logo_Stacked_Full color_RGB 2.png' width={45} height={45}/></div>
+            <div className="relative"><Image alt='logo' src='/img/logo.png' width={45} height={45}/></div>
             <div className="pb-2">Assess</div>
             <div className="border-b-2 px-5 pb-2 font-normal">Explore</div>
             <div className="pb-2">My Forests</div>
