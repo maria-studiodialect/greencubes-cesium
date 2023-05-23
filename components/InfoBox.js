4ebd6b5799ec9f08bec34ad3df8535af3395cae9
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2023-05-23 08:58:08 +0100

    polygon

--- components/InfoBox.js
+++ components/InfoBox.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { RiCloseCircleFill } from 'react-icons/ri';
 
-export default function InfoBox({exploreClick, closeClick}) {
+export default function InfoBox({exploreClick, closeClick, country, location, coordinates, bio, cubes, img}) {
     return (
         <>
         <div className="absolute top-[14%] left-10 z-10">
@@ -11,28 +11,28 @@ export default function InfoBox({exploreClick, closeClick}) {
             <div>
                 <div className="text-xs">Central America</div>
                 <div className="text-xxs opacity-80">Costa Rica</div>
-                <div className="text-xs mt-5">Parcels <span className="ml-4">15</span></div>
+                <div className="text-xs mt-5">Parcels <span className="ml-4">3</span></div>
             </div>
         </div>
         <div className="text-white bg-black/60 backdrop-blur rounded-xl px-4 py-2">
             <div className="flex justify-between space-x-5">
             <div>
                 <div>Costa Rica</div>
-                <div className="text-xs opacity-80">La Selva</div>
+                <div className="text-xs opacity-80">{location ? location : 'La Selva'}</div>
             </div>
-            <div className="text-xxs opacity-40 pt-1">-84.006971, 10.430623</div>
+            <div className="text-xxs opacity-40 pt-1">{coordinates ? coordinates : '-84.006971, 10.430623'}</div>
             </div>
             <div className="bg-white relative h-[18vh] w-[25vw] object-cover rounded-md my-3">
-                <Image src='/img/location-img.svg' fill alt='graphs' className="object-contain"/>
+                <Image src={img ? img : '/img/location-img.svg'} fill alt='graphs' className="object-contain"/>
             </div>
             <div className="grid grid-cols-2 text-center pb-3">
                 <div>
                     <div className="text-xs opacity-80">Biodiversity</div>
-                    <div>High</div>
+                    <div>{bio ? bio : 'High'}</div>
                 </div>
                 <div>
                     <div className="text-xs opacity-80">Cubes</div>
-                    <div>580 Million</div>
+                    <div>{cubes ? cubes : '580 Million'}</div>
                 </div>
             </div>
             <div onClick={exploreClick} className="bg-blue text-center py-1.5 rounded-md mb-[-1em] mx-4 drop-shadow-xl cursor-pointer">Explore</div>
